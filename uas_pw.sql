ec0535329626b1b53cbabbfd27c32719ee309ed4
rikarani <phasyaananta1502@gmail.com>
2022-12-01 05:57:57 +0700

    export db

--- uas_pw.sql
+++ uas_pw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2022 at 01:10 PM
+-- Generation Time: Nov 30, 2022 at 11:56 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -28,13 +28,44 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `hotels` (
-  `id` int(11) NOT NULL,
+  `id_hotel` int(11) NOT NULL,
   `nama_hotel` varchar(255) NOT NULL,
   `alamat_hotel` varchar(255) NOT NULL,
   `harga` varchar(255) NOT NULL,
-  `provinsi` varchar(255) NOT NULL,
+  `lokasi` varchar(255) NOT NULL,
   `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hotels`
+--
+
+INSERT INTO `hotels` (`id_hotel`, `nama_hotel`, `alamat_hotel`, `harga`, `lokasi`, `gambar`) VALUES
+(10, 'Grand Mahkota Hotel', 'Jl. Sidas No. 8', '400.000', 'Pontianak', '6387a449804e1.jpg'),
+(12, 'Kapuas Dharma', 'Jl. Imam Bonjol No. 89', '250.000', 'Pontianak', '6387a4d3c962f.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `id_pemesanan` int(11) NOT NULL,
+  `nama_pemesan` varchar(255) NOT NULL,
+  `nama_hotel` varchar(255) NOT NULL,
+  `tanggal_pemesanan` varchar(255) NOT NULL,
+  `tanggal_mulai` varchar(255) NOT NULL,
+  `tanggal_selesai` varchar(255) NOT NULL,
+  `jumlah_kamar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemesanan`
+--
+
+INSERT INTO `pemesanan` (`id_pemesanan`, `nama_pemesan`, `nama_hotel`, `tanggal_pemesanan`, `tanggal_mulai`, `tanggal_selesai`, `jumlah_kamar`) VALUES
+(1, 'Erika', 'Grand Mahkota Hotel', '2022-12-01 04:34:19', '2022-12-08', '2022-12-02', '22');
 
 -- --------------------------------------------------------
 
@@ -43,7 +74,7 @@ CREATE TABLE `hotels` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `nama_depan` varchar(255) NOT NULL,
   `nama_belakang` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -55,7 +86,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nama_depan`, `nama_belakang`, `username`, `password`, `role`) VALUES
+INSERT INTO `users` (`id_user`, `nama_depan`, `nama_belakang`, `username`, `password`, `role`) VALUES
 (1, 'admin', 'admin', 'admin', '$2y$10$OOernC1XCw8.959Pifr17ehUmfUhKB5rlRun3ntjndJ6nOzWg/Htu', 'admin'),
 (2, 'Erika', 'Maharani', 'erika', '$2y$10$HL4zFViqT9xN5pCl3FyMUewwhr9aNmMuJ7rtcrYgMxtHZWBi8OX6W', 'guest');
 
@@ -67,13 +98,19 @@ INSERT INTO `users` (`id`, `nama_depan`, `nama_belakang`, `username`, `password`
 -- Indexes for table `hotels`
 --
 ALTER TABLE `hotels`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_hotel`);
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`id_pemesanan`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -83,13 +120,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `hotels`
 --
 ALTER TABLE `hotels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_hotel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

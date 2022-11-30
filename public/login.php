068a2574398d0dda004b789076a66e9ed8f9559c
rikarani <phasyaananta1502@gmail.com>
2022-12-01 04:52:05 +0700

    hapus br

--- public/login.php
+++ public/login.php
@@ -93,7 +93,6 @@ if (isset($_POST["login"])) {
         <input type="text" name="username" id="username" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:border-blue-500 focus:outline-none focus:ring-0 peer" placeholder=" " required />
         <label for="username" class="absolute text-base text-slate-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Username</label>
       </div>
-
       <!-- Username -->
 
       <!-- Password -->

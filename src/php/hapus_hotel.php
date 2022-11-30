0e5dc6cf3d7fcd6ad15d20941bc5a5f9c6d19399
rikarani <phasyaananta1502@gmail.com>
2022-12-01 06:10:20 +0700

    ubah link

--- src/php/hapus_hotel.php
+++ src/php/hapus_hotel.php
@@ -6,6 +6,6 @@ $id = $_GET["id"];
 if (hapusHotel($id) > 0) {
   echo "<script>
           alert('Berhasil Menghapus Hotel');
-          window.location.href = 'hotel_list.php';
+          window.location.href = '../../public/admin/hotel_list.php';
         </script>";
 }
--- src/php/hapus_user.php
+++ src/php/hapus_user.php
@@ -6,6 +6,6 @@ $id = $_GET["id"];
 if (hapusUser($id) > 0) {
   echo "<script>
           alert('Berhasil Menghapus User');
-          window.location.href = 'user_list.php';
+          window.location.href = '../../public/admin/user_list.php';
         </script>";
 }
